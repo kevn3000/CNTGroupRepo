4bd66b17c7621f1e472c7a4027957c9cc50c1765
BrandonDocDC <32312852+BrandonDocDC@users.noreply.github.com>
2017-10-09 22:25:17 -0400

    Update MultiThread.java
    updated netstat linux bash command

--- Project1/MultiThread.java
+++ Project1/MultiThread.java
@@ -57,7 +57,7 @@ public class MultiThread extends Thread {
 					break;
 					case "4":
 						System.out.println("Responding to netstat request from the client ");
-						String[] cmdC = {"bash", "-c", "ss -t -a | grep ESTAB | wc -l"};
+						String[] cmdC = {"bash", "-c", "netstat -r"};
 						cmdProc = Runtime.getRuntime().exec(cmdC);
 					break;
 					case "5":
