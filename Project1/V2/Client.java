ec39ba7d5732f325cef2b01924ec7771d31f7ede
BrandonDocDC <32312852+BrandonDocDC@users.noreply.github.com>
2017-10-11 02:07:01 -0400

    Update Client.java
    final version for client fixed.

--- Project1/V2/Client.java
+++ Project1/V2/Client.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class Client {
 	//global vars
     public static String userInput;
-	public static String stopVar = "Stop";
+	public static int clientNumber = 0;
     public static int times, option;
 
     public static void main(String[] args) throws IOException {
@@ -22,21 +22,17 @@ public class Client {
         }//end if
 
 		//When successful, it will establish a socket
-        try{         
+        try{
             Socket clientSocket = new Socket(hostName, portNumber);
-      
+			System.out.println(" -- Connected Client" + clientNumber);
             //While the socket is open, it will listen for host response and display menu after
 			while(true){  
-           
+
 				PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true); // doorstop to keep listening on socket
 				BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); // in is the input from server response
-				BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in)); //user menu input
+				//BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in)); //user menu input
 				Scanner s = new Scanner(System.in); // s var to loop executions
 
-				long start_time = System.currentTimeMillis(); 
-		  
-		 
-				 
 				System.out.println( "1) Server Current Date and Time\n"
 						+           "2) Server Current Uptime\n"
 						+           "3) Server Current Memory Use\n"
@@ -45,27 +41,29 @@ public class Client {
 						+           "6) Server Running Processes\n"
 						+           "7) Quit\n" );
 
-				System.out.println("Select your option: ");
-					option = stdIn.read();
+				System.out.println("Choose an option between 1 and 7: ");
+					option = s.nextInt();
 
-				//System.out.println("How many times would you like to execute this command?");
-				//	times = s.nextInt();
+				//Start Timer
+				long start_time = System.currentTimeMillis();
+
+				//execute each case only once, ask for another option.
 				times = 1;
-				for(int i=0; i<times; i++){   
+				for(int i=0; i<times; i++){
 					// Switch Menu
 					serverResponse=null;
 					switch (option) {
-						case '1':
+						case 1:
 							out.println("1");
 							System.out.println("");
 							System.out.println("The Date and Time: " + in.readLine());
 							break;
-						case '2':
+						case 2:
 							out.println("2");
 							System.out.println("");
 							System.out.println("System has been " + in.readLine());
 							break;
-						case '3':
+						case 3:
 							out.println("3");
 							System.out.println("");
 							System.out.println("=-=-=-=-=-\tServer Memory Response:");
@@ -73,7 +71,7 @@ public class Client {
 								System.out.println(serverResponse);
 							System.out.println("=-=-=-=-=-\tEnd of Response");
 							continue;
-						case '4':
+						case 4:
 							out.println("4");
 							System.out.println("");
 							System.out.println("=-=-=-=-=-\tServer Netstat Response:");
@@ -82,13 +80,13 @@ public class Client {
 							System.out.println("=-=-=-=-=-\tEnd of Response");
 							serverResponse=null;
 							continue;
-						case '5':
+						case 5:
 							out.println("5");
 							System.out.println("");
 							String users = in.readLine();
 							System.out.println("Current Users: " + users);
 							break;
-						case '6':
+						case 6:
 							out.println("6");
 							System.out.println("");
 							System.out.println("=-=-=-=-=-\tServer Processes Response:");
@@ -97,13 +95,13 @@ public class Client {
 							System.out.println("=-=-=-=-=-\tEnd of Response");
 							serverResponse=null;
 							continue;
-						case '7':
+						case 7:
 							out.println("7");
 							System.out.println("");
 							System.out.println("  --  Quitting...");
 							return;
 						default:
-							System.err.println ("  --  Unrecognized option.");
+							System.err.println("  --  Invalid option.");
 						continue;
 					} //end switch
 					//System.out.println("\n  --  Request " + (i+1) + " is done");
@@ -121,11 +119,7 @@ public class Client {
 			}// end while loop
 		}//end try
 
-		//Catchs
-		catch(NumberFormatException e){
-			System.out.print("  --  Please enter valid option!\n");
-			System.exit(1); 
-		}//end catch
+		//Catches bad server connection.
 		catch (UnknownHostException e){
 			System.err.println("  --  Unknown Host: " + hostName );
 			System.exit(1);
@@ -134,9 +128,14 @@ public class Client {
 			System.err.println("  --  Accept from " + hostName + " failed.");
 			System.exit(1);
 		}// end catch
+		//Catchs user input invalid
+		catch(NumberFormatException e){
+			System.out.print("  --  Please enter valid option!\n");
+			System.exit(1); 
+		}//end catch
 		catch (InputMismatchException e){
 			System.err.println ("  --  Unrecognized input "  + e );
 		}//end catch
-		System.out.println(" -- Connected");
+
 	}//end main
 }//end class Client
