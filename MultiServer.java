d386389be0d3c1f4a09b2bb3d291bc6fec70b1cf
Chelsea Saffold <30847326+chelsanator101@users.noreply.github.com>
2017-11-13 16:49:43 -0500

    Add files via upload

--- MultiServer.java
+++ MultiServer.java
@@ -11,6 +11,8 @@ public class MultiServer {
     public static void main(String[] args) throws IOException {
         //Variables 
         int portNumber = Integer.parseInt(args[0]);
+        int numThreads = Integer.parseInt(args[1]);
+        MultiThread[] clientThreads = new MultiThread[numThreads];
 
         //ERROR: If hostName or portNumber is wrong
         if (args.length < 1) {
@@ -24,12 +26,23 @@ public class MultiServer {
             ServerSocket serverSocket = new ServerSocket(portNumber);
             System.out.println("Server started... Listening on port " + portNumber);
             System.out.println("Waiting for clients...");
+            
+            //Create clients threads in the background
+            for(int i = 0; i > numThreads; i++)
+            {
+                clientThreads[i] = new MultiThread(serverSocket.accept());
+            }
+            for(int i = 0; i > numThreads; i++)
+            {
+                clientThreads[i].start();
+            }
 
             //Keep server open to accept multiple clients 
             while (true) {
                 //Create a thread 
                 new MultiThread(serverSocket.accept()).start();
             }//End while 
+            
         }//End try 
         
         //End catches 
